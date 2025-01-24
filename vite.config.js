7e97594a7203c8c6bdcb71cdfcc19df8e28c25a6
Patrick de Klein <patrick.deklein@cito.nl>
2025-01-24 14:56:00 +0100

    possibility to extend properties on item. Introduced info category

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 /** @type {import('vite').UserConfig} */
-export default {
-  optimizeDeps: {
-    noDiscovery: true,
-    include: undefined,
+import { defineConfig } from "vite";
+
+const fullReloadAlways = {
+  name: "full-reload-always",
+  handleHotUpdate({ server }) {
+    server.ws.send({ type: "full-reload" });
+    return [];
   },
 };
+
+export default defineConfig({
+  plugins: [fullReloadAlways],
+  server: {
+    watch: {
+      // Allow watching files in `node_modules`
+      ignored: [
+        '!**/node_modules/@citolab/qti-components/**',
+        '!**/node_modules/@citolab/qti-extended/**']
+    },
+  },
+  optimizeDeps: {
+    noDiscovery: true,
+  },
+});
